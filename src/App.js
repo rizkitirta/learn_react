5930a0ea2452f4873be7a145e2ffa2a8348bdc98
rizkitirta <rizkibuwono0@gmail.com>
2021-10-20 20:24:24 +0700

    react functional

--- src/App.js
+++ src/App.js
@@ -1,87 +1,67 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 
-class App extends Component {
-  constructor() {
-    super();
+function App() {
+  const [firts_name, setFirts_name] = useState("");
+  const [last_name, setLast_name] = useState("");
+  const [name, setName] = useState("");
 
-    this.state = {
-      firstName: "",
-      lastName: "",
-      name: "",
-    };
-  }
-
-  changeHandler = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  submitHandler = (e) => {
+  const submitHandler = (e) => {
     e.preventDefault();
-    this.setState({
-      name: `${this.state.firstName} ${this.state.lastName}`,
-    });
+    setName(`${firts_name} ${last_name}`);
 
-    this.state.firstName = "";
-    this.state.lastName = "";
+    setFirts_name("");
+    setLast_name("");
   };
 
-  render() {
-    const { name } = this.state;
-    return (
-      <div className="p-5">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4">
-              <div className="card">
-                <div className="card-header">Learn React</div>
-                <div className="card-body">
-                  <form onSubmit={this.submitHandler}>
-                    <div className="mb-4">
-                      <label htmlFor="firstName" className="form-label">
-                        First Name
-                      </label>
-                      <input
-                        type="text"
-                        onChange={this.changeHandler}
-                        value={this.state.firstName}
-                        name="firstName"
-                        id="firstName"
-                        className="form-control"
-                      ></input>
-                    </div>
-                    <div className="mb-4">
-                      <label htmlFor="lastName" className="form-label">
-                        Last Name
-                      </label>
-                      <input
-                        type="text"
-                        onChange={this.changeHandler}
-                        value={this.state.lastName}
-                        name="lastName"
-                        id="lastName"
-                        className="form-control"
-                      ></input>
-                    </div>
-                    <button
-                      type="submit"
-                      className="btn btn-outline-primary btn-block"
-                    >
-                      Submit
-                    </button>
-                  </form>
-                </div>
-                <div className="card-footer text-muted">
-                  My Name Is {name ? name : ""}
-                </div>
+  return (
+    <div className="p-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-header">Learn React Function</div>
+              <div className="card-body">
+                <form onSubmit={submitHandler}>
+                  <div className="mb-2">
+                    <label className="form-label" htmlFor="first_name">
+                      First Name
+                    </label>
+                    <input
+                      type="text"
+                      name="first_name"
+                      id="first_name"
+                      className="form-control"
+                      value={firts_name}
+                      onChange={(e) => setFirts_name(e.target.value)}
+                    />
+                  </div>
+                  <div className="mb-2">
+                    <label className="form-label" htmlFor="last_name">
+                      Last Name
+                    </label>
+                    <input
+                      type="text"
+                      name="last_name"
+                      id="last_name"
+                      className="form-control"
+                      value={last_name}
+                      onChange={(e) => setLast_name(e.target.value)}
+                    />
+                  </div>
+                  <button type="submit" className="btn btn-primary">
+                    Submit
+                  </button>
+                </form>
+              </div>
+              <div className="card-footer">
+                My Name Is {name ? name : "..."}
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
