92b29e8518bc5c6adf7e4c83704b664b8bbe2552
rizkitirta <rizkibuwono0@gmail.com>
2021-10-20 15:25:15 +0700

    form handling react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,87 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-class App extends Component{
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      firstName: "",
+      lastName: "",
+      name: "",
+    };
+  }
+
+  changeHandler = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  submitHandler = (e) => {
+    e.preventDefault();
+    this.setState({
+      name: `${this.state.firstName} ${this.state.lastName}`,
+    });
+
+    this.state.firstName = "";
+    this.state.lastName = "";
+  };
+
   render() {
+    const { name } = this.state;
     return (
-      <div>
-        <h1>Hello</h1>
-        <h2>world</h2>
+      <div className="p-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <div className="card">
+                <div className="card-header">Learn React</div>
+                <div className="card-body">
+                  <form onSubmit={this.submitHandler}>
+                    <div className="mb-4">
+                      <label htmlFor="firstName" className="form-label">
+                        First Name
+                      </label>
+                      <input
+                        type="text"
+                        onChange={this.changeHandler}
+                        value={this.state.firstName}
+                        name="firstName"
+                        id="firstName"
+                        className="form-control"
+                      ></input>
+                    </div>
+                    <div className="mb-4">
+                      <label htmlFor="lastName" className="form-label">
+                        Last Name
+                      </label>
+                      <input
+                        type="text"
+                        onChange={this.changeHandler}
+                        value={this.state.lastName}
+                        name="lastName"
+                        id="lastName"
+                        className="form-control"
+                      ></input>
+                    </div>
+                    <button
+                      type="submit"
+                      className="btn btn-outline-primary btn-block"
+                    >
+                      Submit
+                    </button>
+                  </form>
+                </div>
+                <div className="card-footer text-muted">
+                  My Name Is {name ? name : ""}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
